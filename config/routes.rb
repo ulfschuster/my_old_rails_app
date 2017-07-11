23c13af288d11e53dca1369194c02a1aafbd1165
Ulf Schuster <ulf.schuster@gmail.com>
2017-07-11 10:55:53 +0200

    3.8 added pages about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+  root 'static_pages#index' #Sets the root of the rails app to the controller static_pages and the page index. The controller for the static page needs to be referenced in the routes.rb file like this. Otherwise, the user cannot access the page.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
