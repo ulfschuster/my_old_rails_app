8ba515e8abd1b4e10c5262ba5d876ec152ffb214
Ulf Schuster <ulf.schuster@gmail.com>
2017-07-25 10:55:21 +0200

    4.2 image tag causes error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    @test_variable = 42
   end
 
   # GET /products/1
@@ -69,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
+      params.require(:product).permit(:name, :description, :image_url, :colour, :price)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716115249) do
+ActiveRecord::Schema.define(version: 20170721084731) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170716115249) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "colour"
+    t.string "price"
   end
 
 end
