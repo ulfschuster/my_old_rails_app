47c618d3e945ddd18cca88a686df0065daff638b
Ulf Schuster <ulf.schuster@gmail.com>
2017-07-26 10:36:42 +0200

    4.3 Trying to fix issue after deploy to heroku --  Controllers:Added Landing Page, created orders_controller.rb and orders folder with empty index.html.erb and show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  get 'static_pages/landing_page'
   
   root 'static_pages#landing_page' #Sets the root of the rails app to the controller static_pages and the landing page. The controller for the static page needs to be referenced in the routes.rb file like this. Otherwise, the user cannot access the page.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
