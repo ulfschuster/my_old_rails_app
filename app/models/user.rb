caa85ee53af44cae0eba354f457384ef1435a367
Ulf Schuster <ulf.schuster@gmail.com>
2017-09-08 13:44:39 +0200

    5.4 Reinstallled devise gem; rails db:migrate runs into errors

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+#class User < ApplicationRecord
+	#has many :orders
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
