7f98756668972340a8fd1a5e7f24ac3e825454e1
Ulf Schuster <ulf.schuster@gmail.com>
2017-09-21 19:39:32 +0200

    5.7 Created models/comment.rb and modified files accordingly; created new branch 'comment'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
+	has_many :comments
+	has_many :orders #Added in 5.7 according to my sketch from 5.6
 	def self.search(search_term)
 		Product.where("name LIKE ?", "%#{search_term}%")
+	end
+	def highest_rating_comment
+		comments.rating_desc.first
 	end
 end
